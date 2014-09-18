00a68b239ccf03a0bec6b4a2243b8e7c258738ac
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-09-18 16:45:28 -0600

    Fixed naive code

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from widgy_blog.admin import BlogAdmin
+from widgy_blog.models import Blog
+
+from blog.models import News
+
+
+class NewsAdmin(BlogAdmin):
+    layout_proxy_fields = [
+        'title',
+        'slug',
+        'date',
+        'summary',
+        'description',
+        'keywords',
+        'page_title',
+    ]
+    list_display = ['title']
+    fieldsets = [
+        (None, {
+            'fields': [
+                'title', 'date', 'summary', 'content',
+            ],
+        }),
+        ('Meta', {
+            'fields': ['description', 'keywords', 'slug', 'page_title'],
+            'classes': ['collapse', 'grp-collapse', 'collapse-closed',
+                        'collapsed'],
+        }),
+    ]
+
+admin.site.unregister(Blog)
+admin.site.register(News, NewsAdmin)
--- widgy_demo/site.py
+++ widgy_demo/site.py
@@ -5,7 +5,11 @@ from widgets.models import HomepageLayout, WhateverpageLayout
 
 class WidgySite(WidgySite):
     def valid_root_of(self, owner_class, root_class, root_choices):
-        return issubclass(root_class, (HomepageLayout, WhateverpageLayout,))
+        from widgy.contrib.widgy_mezzanine.models import WidgyPage
+        if issubclass(owner_class, WidgyPage):
+            return issubclass(root_class, (HomepageLayout, WhateverpageLayout,))
+        else:
+            return super(WidgySite, self).valid_root_of(owner_class, root_class, root_choices)
 
 
 site = WidgySite()
