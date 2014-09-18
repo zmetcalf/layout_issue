1de1bc16fb12d6eb433fe7deb22f141b3338c6bf
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-09-18 15:36:14 -0600

    Put tupel in sites

--- widgy_demo/site.py
+++ widgy_demo/site.py
@@ -1,7 +1,11 @@
 from widgy.site import WidgySite
 
+from widgets.models import HomepageLayout, WhateverpageLayout
+
 
 class WidgySite(WidgySite):
-    pass
+    def valid_root_of(self, owner_class, root_class, root_choices):
+        return issubclass(root_class, (HomepageLayout, WhateverpageLayout,))
+
 
 site = WidgySite()
